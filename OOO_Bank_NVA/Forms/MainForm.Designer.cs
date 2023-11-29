bfe7bf8ff7cea603b99542cd4fb65d4e9a15e70e
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-11-30 02:21:57 +0300

    Продумал логику

--- OOO_Bank_NVA/Forms/MainForm.Designer.cs
+++ OOO_Bank_NVA/Forms/MainForm.Designer.cs
@@ -38,8 +38,11 @@
             this.tabUsers = new System.Windows.Forms.TabPage();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.materialTabControl1.SuspendLayout();
+            this.tabUsers.SuspendLayout();
             this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -50,6 +53,7 @@
             this.materialTabControl1.Controls.Add(this.tabSettings);
             this.materialTabControl1.Depth = 0;
             this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.materialTabControl1.ImageList = this.imageList1;
             this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
             this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
@@ -102,6 +106,8 @@
             // 
             // tabUsers
             // 
+            this.tabUsers.Controls.Add(this.dataGridView1);
+            this.tabUsers.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabUsers.ImageKey = "User.png";
             this.tabUsers.Location = new System.Drawing.Point(4, 39);
             this.tabUsers.Name = "tabUsers";
@@ -109,6 +115,7 @@
             this.tabUsers.Size = new System.Drawing.Size(1506, 709);
             this.tabUsers.TabIndex = 0;
             this.tabUsers.Text = "Пользователи";
+            this.tabUsers.ToolTipText = "Привет";
             this.tabUsers.UseVisualStyleBackColor = true;
             // 
             // statusStrip
@@ -116,18 +123,29 @@
             this.statusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip.Location = new System.Drawing.Point(3, 784);
+            this.statusStrip.Location = new System.Drawing.Point(3, 782);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.statusStrip.Size = new System.Drawing.Size(1514, 32);
+            this.statusStrip.Size = new System.Drawing.Size(1514, 34);
             this.statusStrip.TabIndex = 0;
             this.statusStrip.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(186, 25);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(231, 27);
             this.toolStripStatusLabel1.Text = "Статус: Пользователь";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1503, 597);
+            this.dataGridView1.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -146,8 +164,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OOO Банк NVA";
             this.materialTabControl1.ResumeLayout(false);
+            this.tabUsers.ResumeLayout(false);
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,5 +183,6 @@
         private System.Windows.Forms.TabPage tabUsers;
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
