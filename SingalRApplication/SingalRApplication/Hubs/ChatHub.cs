bf0180fae97416979b3fad52f0569b5639c14eba
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-12-03 00:18:22 +0300

    Добавил отдельный проект по Asp Net Core SingleR

---
+++ SingalRApplication/SingalRApplication/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SingalRApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        int i = 0;
+        public async Task Send(string message, string username = "")
+        {
+            if(username == "")
+            {
+                username = "Аноним" + i++;
+            }
+            await this.Clients.All.SendAsync("Send", message, username);
+        }
+    }
+}
---
+++ SingalRApplication/SingalRApplication/Hubs/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace SingalRApplication.Hubs
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
