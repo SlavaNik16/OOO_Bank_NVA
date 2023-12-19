d62bf954af9a97c334008ff163ef90907cfd0072
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-12-20 01:07:22 +0300

    Добавил иконку

--- OOO_Bank_NVA/Properties/Resources.Designer.cs
+++ OOO_Bank_NVA/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace OOO_Bank_NVA.Properties {
         }
         
         /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon IconBank {
+            get {
+                object obj = ResourceManager.GetObject("IconBank", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap photoEmpty {
